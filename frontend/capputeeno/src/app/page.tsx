708bd8af3f3c6ec645579974c3e54dc5a11da9c2
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-07-18 18:41:26 -0300

    refactor: removing `page.module.css`

--- frontend/capputeeno/src/app/page.tsx
+++ frontend/capputeeno/src/app/page.tsx
@@ -1,16 +1,26 @@
 "use client";
 import { FilterBar } from "@/components/filter-bar";
-import styles from "./page.module.css";
 import { ProductsList } from "@/components/products-list";
 import { QueryClient } from "@tanstack/react-query";
+import styled from "styled-components";
 
+const PageWrapper = styled.main`
+  background-color: var(--bg-primary);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 34px 160px;
+  min-height: 100vh;
+  background-color: var(--bg-primary);
+  min-height: 100vh;
+`;
 export default function Home() {
   const client = new QueryClient();
 
   return (
-    <main className={styles.main}>
+    <PageWrapper>
       <FilterBar />
       <ProductsList />
-    </main>
+    </PageWrapper>
   );
 }
