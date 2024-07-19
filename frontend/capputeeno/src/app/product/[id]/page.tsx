d915767e7f417832d80a272450e20a2087fbfe7b
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-07-19 15:59:31 -0300

    feat: finish product page

--- frontend/capputeeno/src/app/product/[id]/page.tsx
+++ frontend/capputeeno/src/app/product/[id]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import BackIcon from "@/components/icons/back-icon";
+import BagIcon from "@/components/icons/bag-icon";
+import CartIcon from "@/components/icons/cart-icon";
 import { useProduct } from "@/hooks/useProduct";
 import styled from "styled-components";
 
@@ -28,8 +30,77 @@ const BackButton = styled.button`
 
 const ProductInfo = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
+  margin-top: 22px;
+
+  img {
+    max-width: 640px;
+    width: 50%;
+    border-radius: 4px;
+  }
+`;
+const SectionProduct = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: 32px;
+  font-family: inherit;
+  text-align: left;
+  color: var(--text-dark2);
+  h2 {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+  }
+
+  h1 {
+    font-size: 32px;
+    font-weight: 300;
+    line-height: 48px;
+    margin-top: 12px;
+  }
+  h3 {
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 24px;
+    text-transform: uppercase;
+    margin-top: 58px;
+  }
+  span {
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 30px;
+    margin-top: 4px;
+  }
+  p {
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 18px;
+    margin-top: 24px;
+  }
+  p:nth-of-type(2) {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 21px;
+    margin-top: 8px;
+  }
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+    width: 448px;
+    border-radius: 4px;
+    border: none;
+    background-color: var(--button-primary);
+    font-family: inherit;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 24px;
+    text-transform: uppercase;
+    color: var(--text-button);
+    padding: 10px;
+  }
 `;
 
 export default function Product({ params }: { params: { id: string } }) {
@@ -44,19 +115,26 @@ export default function Product({ params }: { params: { id: string } }) {
       </BackButton>
       <ProductInfo>
         <img src={product?.image_url} alt="Imagem do produto" />
-
-        <div>
-          <h2>{product?.category}</h2>
-          <h1>{product?.name}</h1>
-          <span>R$ {product?.price_in_cents}</span>
-          <p>
-            *Frete de R$40,00 para todo o Brasil. Grátis para compras acima de
-            R$900,00.
-          </p>
-          <h3>Descrição</h3>
-          <p>{product?.description}</p>
-          <button>Comprar</button>
-        </div>
+        <SectionProduct>
+          <div>
+            <h2>{product?.category}</h2>
+            <h1>{product?.name}</h1>
+            <span>
+              {`R$ ${
+                product?.price_in_cents ? product.price_in_cents / 100 : 0
+              }`.replace(".", ",")}
+            </span>
+            <p>
+              *Frete de R$40,00 para todo o Brasil. Grátis para compras acima de
+              R$900,00.
+            </p>
+            <h3>Descrição</h3>
+            <p>{product?.description}</p>
+          </div>
+          <button>
+            <BagIcon /> Adicionar ao carrinho
+          </button>
+        </SectionProduct>
       </ProductInfo>
     </ProductContainer>
   );
--- frontend/capputeeno/src/components/default-page-layout.tsx
+++ frontend/capputeeno/src/components/default-page-layout.tsx
@@ -6,5 +6,4 @@ export const DefaultPageLayout = styled.div`
 
   padding: 12px 24px;
   background-color: var(--bg-primary);
-  min-height: 100vh;
 `;
